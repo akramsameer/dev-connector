8522e14986a1ca547962e76cc45d9d1c57929a39
Akram <akram.sameer99@gmail.com>
2018-08-16 16:27:31 +0200

    Finishing module 03

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://akram:as2255@ds123372.mlab.com:23372/dev-connector"
+  mongoURI: 'mongodb://akram:as2255@ds123372.mlab.com:23372/dev-connector',
+  secretOrKey: 'ksjgu48urer9283403jdfodsfu458ukdlnf'
 };
