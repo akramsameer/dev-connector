22d6f5b92ae59aa32971242f61787c43a9a9b2cb
Akram <akram.sameer99@gmail.com>
2018-08-25 04:56:08 +0200

    Finishing Module 8  Adding Client-side

---
+++ my-notes.js
@@ -0,0 +1,9 @@
+/*
+ *  -using proxy in package.json enabing you for calling 
+ *   axios.get(/api/somthing) rather than writing the whole url
+ *  
+ *  - we use concurrently to run two command in the same time 
+ *      run ther server and the client
+ * 
+ *  - Adding noValidate to form tag to prevent built-in validation for custom one
+ */
